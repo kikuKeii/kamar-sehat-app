ed5b533aa7743f6fa2f51c51105eeebfe92c9262
kikukeii <miftakhuddinfalaki@gmail.com>
2023-04-27 17:01:14 +0700

    update nav

--- include/navbar.jsx
+++ include/navbar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/no-html-link-for-pages */
 const Navbar = () => {
     return (
@@ -13,6 +15,7 @@ const Navbar = () => {
                         <li className="nav-item"><a href="/cek" className="nav-link link-dark" aria-current="page">Cek Kamar</a></li>
                         <li className="nav-item"><a href="/#product" className="nav-link link-dark">Product</a></li>
                         <li className="nav-item"><a href="/#about" className="nav-link link-dark">About</a></li>
+                        <li className="nav-item"><a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fkamar-sehat.vercel.app&count_bg=%230042FF&title_bg=%23555555&icon=next-dot-js.svg&icon_color=%23E7E7E7&title=Visited&edge_flat=false" /></a></li>
                     </ul>
                 </header>
             </div>
